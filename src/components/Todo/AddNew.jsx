a1ac39965349886b74d3fe93c607ef80dd1b352f
Reza Zahedi <r.zahedi@gmail.com>
2024-11-11 12:12:10 -0800

    Add styles to AddNew component

--- src/components/Todo/AddNew.jsx
+++ src/components/Todo/AddNew.jsx
@@ -1,6 +1,8 @@
+import './style.css'
+
 function AddNew() {
   return (
-    <form>
+    <form className="add-new">
       <label htmlFor="myInput">Add new todo</label>
       <div>
         <input id="myInput" type="text" placeholder="Add new todo" />
