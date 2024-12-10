898e35141a61920a277801bb7de775f98072e2a6
Reza Zahedi <r.zahedi@gmail.com>
2024-12-10 08:58:03 -0800

    Save items in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import List from './components/Todo/List'
 import AddNew from './components/Todo/AddNew'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  }, [todoList])
 
   const addTodo = (newTodo) => {
     setTodoList((prevData) => [...prevData, {id: Date.now(), title: newTodo} ])
