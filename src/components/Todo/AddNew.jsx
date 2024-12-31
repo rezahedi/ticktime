814dc5c118ea5368b995dc0f7a3579c303226b10
Reza Zahedi <r.zahedi@gmail.com>
2024-12-30 22:21:04 -0800

    Add newTodo with airtable api fetch

--- src/components/Todo/AddNew.jsx
+++ src/components/Todo/AddNew.jsx
@@ -6,12 +6,55 @@ function AddNew( props ) {
   const { onAddNew } = props
 
   const [title, setTitle] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
 
-  const handleFormSubmission = (e) => {
+  const postNewTodo = async () => {
+    setError('')
+    setIsLoading(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      url: `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`,
+    }
+
+    // Create the new Todo's object in fetch's body payload
+    options.body = JSON.stringify({
+      records: [{
+        fields: {
+          title: title,
+        }
+      }]
+    })
+
+    try {
+      const response = await fetch(options.url, options)
+      if ( !response.ok ) {
+        throw new Error(`Error: ${response.status}`)
+      }
+
+      const data = await response.json()
+      // TODO: Pass the data.records[0] object to the parent component to update the list state
+      // onAddNew( title )
+      console.log('New:', data)
+
+      setTitle('')
+      setIsLoading(false);
+
+    } catch (error) {
+      setIsLoading(false)
+      setError("Something went wrong, try again")
+      console.error(error)
+    }
+  }
+
+  const handleFormSubmission = async (e) => {
     e.preventDefault()
-
-    onAddNew( title )
-    setTitle('')
+    await postNewTodo()
   }
 
   const handleTitleChange = (e) => {
@@ -20,12 +63,20 @@ function AddNew( props ) {
   }
 
   return (
-    <form className="add-new" onSubmit={handleFormSubmission}>
-      <InputWithLabel title={title} handleTitleChange={handleTitleChange}>
-        Title
-      </InputWithLabel>
-      <button type="submit">Add</button>
-    </form>
+    <>
+      <form className="add-new" onSubmit={handleFormSubmission}>
+        <InputWithLabel title={title} handleTitleChange={handleTitleChange}>
+          Title
+        </InputWithLabel>
+        <button type="submit">
+          {isLoading
+            ? <div className="loader"></div>
+            : <>Add</>
+          }
+        </button>
+      </form>
+      {error && <p className='error'>{error}</p>}
+    </>
   )
 }
 
