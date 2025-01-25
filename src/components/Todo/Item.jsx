a891257aa33730ce40ede950caf8f963bf044768
Reza Zahedi <r.zahedi@gmail.com>
2025-01-25 15:08:11 -0800

    Add deadline tag to items

--- src/components/Todo/Item.jsx
+++ src/components/Todo/Item.jsx
@@ -1,8 +1,29 @@
 import styles from './Item.module.css'
 
+const convertDateFormat = (date) => {
+  const [year, month, day] = date.split('-')
+  return `${month}/${day}/${year}`
+}
+
+/**
+ * 
+ * @param {string} date format 'mm/dd/yyyy'
+ * @returns number
+ */
+const calculateRemainedDays = (date) => {
+  const inputDate = new Date( convertDateFormat(date) + ' 11:59:59' )
+  const currentDate = new Date()
+  const differenceInMs = inputDate - currentDate
+  const differenceInDays = Math.round(differenceInMs / (24 * 60 * 60 * 1000));
+  return differenceInDays
+}
+
 /* eslint-disable react/prop-types */
 function Item( props ) {
   const { todoItem, onRemoveTodo } = props
+  const days = calculateRemainedDays(todoItem.deadline)
+  const deadlineMessage = days > 0 ? `${days} days remained` : `${days} days passed`
+
 
   const handleRemoveClick = () => {
     onRemoveTodo(todoItem)
@@ -10,7 +31,15 @@ function Item( props ) {
   
   return (
     <div className={styles.item}>
-      <span className={todoItem.temp ? styles.temporary : ''}>{todoItem.icon} {todoItem.title}</span>
+      <span className={todoItem.temp ? styles.temporary : ''}>
+        <span className={todoItem.completedAt && styles.completed}>
+          {todoItem.icon}
+          {todoItem.title}
+        </span>
+        {(days>0 || days<0) && !todoItem.completedAt &&
+          <i className={days>0 ? styles.remainedDeadline : styles.passedDeadline}>{deadlineMessage}</i>
+        }
+      </span>
       <button onClick={handleRemoveClick}>x</button>
     </div>
   )
