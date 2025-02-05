03d38f6f3a5b653745a276d2f91e093af7e76993
Reza Zahedi <r.zahedi@gmail.com>
2025-02-05 11:32:01 -0800

    Sort by Airtable fields

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -31,7 +31,14 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
         Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
       },
     }
-    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?sort[0][field]=completedAt&sort[0][direction]=asc&sort[1][field]=deadline&sort[1][direction]=asc`
+    
+    const params = new URLSearchParams()
+    params.append('sort[0][field]', 'completedAt')
+    params.append('sort[0][direction]', 'asc')
+    params.append('sort[1][field]', 'deadline')
+    params.append('sort[1][direction]', 'asc')
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?${params}`
 
     try {
       const response = await fetch(url, options)
