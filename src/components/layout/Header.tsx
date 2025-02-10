0591a315a892985e1b04c086fc23782048109364
Reza Zahedi <r.zahedi@gmail.com>
2025-02-10 14:59:09 -0800

    Redesign header with new logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,21 @@
 import { Link, NavLink } from 'react-router-dom'
+import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <header style={{width:'100%', borderBottom:'1px solid white', backgroundColor:'#131314'}}>
+    <header>
       <div className='container' style={{padding:'0 1rem'}}>
-        <h1>
-          <Link to="/">Tick Time</Link>
-        </h1>
-        <nav style={{display:'flex', gap:'1rem'}}>
+        <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', margin:'1rem 0'}}>
+          <Link className={styles.logo} to="/">
+            <img src='./public/ticktime.svg' />
+            <div>
+              <h1>TickTime</h1>
+              <span>Time to tick the todos!</span>
+            </div>
+          </Link>
+          <Link to='https://github.com/rezahedi/ticktime' target='_blank' style={{background:'white',color:'black', padding:'.5rem 1.2rem', borderRadius:'3rem'}}>Github</Link>
+        </div>
+        <nav>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/new'>Add New</NavLink>
         </nav>
