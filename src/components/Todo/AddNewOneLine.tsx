e7e5c403872c4fbd9d2ce825c97752af77c251f3
Reza Zahedi <r.zahedi@gmail.com>
2025-02-13 20:45:52 -0800

    Create AddNew OneLine component

---
+++ src/components/Todo/AddNewOneLine.tsx
@@ -0,0 +1,43 @@
+import styles from './AddNew.module.css'
+import { useData } from '../../context/DataContext'
+import SelectIcon from './SelectIcon'
+
+function AddNewOneLine() {
+  const {onAddNew, onAddError} = useData()
+
+  const todayDate = new Date().toLocaleDateString("en-CA"); // en-CA return yyyy-mm-dd format that input type date needs!
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [error, setError] = useState('')
+
+  const handleFormSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // Using formData API, controlled components cause re-render
+    const formElement = e.target as HTMLFormElement;
+    const formData = new FormData( formElement );
+    const {title, icon} = Object.fromEntries(formData.entries()) as { [key: string]: string }
+
+    const res = await onAddNew({
+      title,
+      icon,
+      description:'',
+      deadline: todayDate,
+    })
+    res && formElement.reset()
+  }
+
+  return (
+    <>
+      <form className={styles.form} onSubmit={handleFormSubmission}>
+        <div className={styles.mainRow}>
+          <SelectIcon name='icon' />
+          <input name='title' type="text" maxLength={40} required={true} placeholder="Ex: Do cleaning ..." />
+          <button type="submit">Create</button>
+        </div>
+      </form>
+      {onAddError && <p className='error'>{onAddError}</p>}
+    </>
+  )
+}
+
+export default AddNewOneLine
